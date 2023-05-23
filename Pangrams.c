597e1f7232d7ce86101056e32a7683255be2a416
Esraa El-Shoaiby <133651070+EsraaShoaib0@users.noreply.github.com>
2023-05-23 18:28:10 +0300

    Create Pangrams.c

---
+++ Pangrams.c
@@ -0,0 +1,137 @@
+#include <assert.h>
+#include <ctype.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* readline();
+
+/*
+ * Complete the 'pangrams' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts STRING s as parameter.
+ */
+
+/*
+ * To return the string from the function, you should either do static allocation or dynamic allocation
+ *
+ * For example,
+ * char* return_string_using_static_allocation() {
+ *     static char s[] = "static allocation of string";
+ *
+ *     return s;
+ * }
+ *
+ * char* return_string_using_dynamic_allocation() {
+ *     char* s = malloc(100 * sizeof(char));
+ *
+ *     s = "dynamic allocation of string";
+ *
+ *     return s;
+ * }
+ *
+ */
+char* pangrams(char* s) {
+     int *arr = calloc(26, sizeof(int));
+    int idx;
+    char *pangram = malloc(sizeof(40));
+    char *pangram_no = malloc(sizeof(40));
+    pangram = "pangram";
+    pangram_no = "not pangram";
+    for(int i=0; i<strlen(s); i++){
+        if(s[i]>='A' && s[i]<='Z'){
+            idx = s[i] - 'A';
+            arr[idx] = 1;
+
+        }
+        else if(s[i]>='a' && s[i]<='z'){
+            idx = s[i] - 'a';
+             arr[idx] = 1;
+
+        }
+    }
+    int pan = 1;
+    for(int i=0; i<26 ;i++){
+        if(arr[i]==0)
+            pan = 0;
+    }
+    if(pan){
+        
+        return pangram;
+    }
+return pangram_no; 
+
+}
+
+int main()
+{
+    FILE* fptr = fopen(getenv("OUTPUT_PATH"), "w");
+
+    char* s = readline();
+
+    char* result = pangrams(s);
+
+    fprintf(fptr, "%s\n", result);
+
+    fclose(fptr);
+
+    return 0;
+}
+
+char* readline() {
+    size_t alloc_length = 1024;
+    size_t data_length = 0;
+
+    char* data = malloc(alloc_length);
+
+    while (true) {
+        char* cursor = data + data_length;
+        char* line = fgets(cursor, alloc_length - data_length, stdin);
+
+        if (!line) {
+            break;
+        }
+
+        data_length += strlen(cursor);
+
+        if (data_length < alloc_length - 1 || data[data_length - 1] == '\n') {
+            break;
+        }
+
+        alloc_length <<= 1;
+
+        data = realloc(data, alloc_length);
+
+        if (!data) {
+            data = '\0';
+
+            break;
+        }
+    }
+
+    if (data[data_length - 1] == '\n') {
+        data[data_length - 1] = '\0';
+
+        data = realloc(data, data_length);
+
+        if (!data) {
+            data = '\0';
+        }
+    } else {
+        data = realloc(data, data_length + 1);
+
+        if (!data) {
+            data = '\0';
+        } else {
+            data[data_length] = '\0';
+        }
+    }
+
+    return data;
+}
